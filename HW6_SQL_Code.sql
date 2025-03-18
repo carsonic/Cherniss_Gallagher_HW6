ce697053e97fc5aa0f3162cd7dd18275a4d47291
carsonic <156849942+carsonic@users.noreply.github.com>
2025-03-17 21:03:34 -0400

    Add code for question 2

--- HW6_SQL_Code.sql
+++ HW6_SQL_Code.sql
@@ -1,4 +1,14 @@
+-- Question 1
 SELECT origin, AVG(temp) AS mean_temperature
 FROM flights.main.weather
 GROUP BY origin
 
+-- Question 2
+SELECT origin AS airport,
+       COUNT(*) / COUNT(DISTINCT day) AS avg_flights_per_day
+FROM flights.main.flights
+WHERE month = 1
+GROUP BY origin
+ORDER BY avg_flights_per_day DESC;
+
+-- Question 3
