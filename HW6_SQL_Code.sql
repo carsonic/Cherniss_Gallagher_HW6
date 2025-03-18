e55bcea372944c094aef711c36fdf212cd6db9d6
carsonic <156849942+carsonic@users.noreply.github.com>
2025-03-17 21:23:46 -0400

    Add code for question 3

--- HW6_SQL_Code.sql
+++ HW6_SQL_Code.sql
@@ -12,3 +12,9 @@ GROUP BY origin
 ORDER BY avg_flights_per_day DESC;
 
 -- Question 3
+SELECT DISTINCT ON (origin) origin AS airport, -- Had to google the "DISTINCT ON" thing, let me know if you know an easier way
+       dest AS top_destination,
+       COUNT(*) AS flight_count
+FROM flights.main.flights
+GROUP BY origin, dest
+ORDER BY origin, flight_count;
